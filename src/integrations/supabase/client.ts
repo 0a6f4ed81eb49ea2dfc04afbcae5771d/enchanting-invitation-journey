d19825ecbf17d058f293cc06a244db6165152374
0a6f4ed81eb49ea2dfc04afbcae5771d <0a6f4ed81eb49ea2dfc04afbcae5771d@FireMail.com.br>
2025-01-09 00:45:38 -0300

    fix anon api key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from './types'
 
 const supabaseUrl = 'https://dmodmuwamavwggpshtsu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtb2RtdXdhbWF2d2dncHNodHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0OTY5NzAsImV4cCI6MjAyNTA3Mjk3MH0.Wd_jqxGhVJKBKEQHOQcYQqOXEZJGqEBtbFGxZLxYPBE'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtb2RtdXdhbWF2d2dncHNodHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNTUyMDksImV4cCI6MjA1MTkzMTIwOX0.V4Xv3tNkDpGzoGCZQAs1fm2wA-rVc59DuboDZzGaM4U'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
